9207dea67fdfa8e38bcace886894addaf39115eb
Olle Fredriksson <fredriksson.olle@gmail.com>
2024-05-08 18:49:50 +0200

    Use blink

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,5 +1,3 @@
-use typed_arena::Arena;
-
 #[derive(Copy, Clone)]
 pub struct Index {
     pub int: usize,
@@ -22,28 +20,33 @@ pub type Type<'a> = Term<'a>;
 pub type TermRef<'a> = &'a Term<'a>;
 pub type TypeRef<'a> = &'a Type<'a>;
 
-pub struct Builder<'a> {
-    arena: Arena<Term<'a>>,
+pub struct Builder {
+    arena: blink_alloc::Blink,
 }
 
-impl<'a> Builder<'a> {
-    pub fn variable(&'a mut self, index: Index) -> TermRef<'a> {
-        self.arena.alloc(Term::Variable(index))
+impl Builder {
+    pub fn variable<'a>(&'a self, index: Index) -> TermRef<'a> {
+        self.arena.put_no_drop(Term::Variable(index))
     }
 
-    pub fn literal(&'a mut self, literal: Literal) -> TermRef<'a> {
-        self.arena.alloc(Term::Literal(literal))
+    pub fn literal<'a>(&'a self, literal: Literal) -> TermRef<'a> {
+        self.arena.put_no_drop(Term::Literal(literal))
     }
 
-    pub fn lambda(&'a mut self, type_: TypeRef<'a>, body: TermRef<'a>) -> TermRef<'a> {
-        self.arena.alloc(Term::Lambda(type_, body))
+    pub fn lambda<'a>(&'a self, type_: TypeRef<'a>, body: TermRef<'a>) -> TermRef<'a> {
+        self.arena.put_no_drop(Term::Lambda(type_, body))
     }
 
-    pub fn pi(&'a mut self, domain: TypeRef<'a>, target: TermRef<'a>) -> TermRef<'a> {
-        self.arena.alloc(Term::Pi(domain, target))
+    pub fn pi<'a>(&'a self, domain: TypeRef<'a>, target: TermRef<'a>) -> TermRef<'a> {
+        self.arena.put_no_drop(Term::Pi(domain, target))
     }
 
-    pub fn application(&'a mut self, function: TermRef<'a>, argument: TermRef<'a>) -> TermRef<'a> {
-        self.arena.alloc(Term::Application(function, argument))
+    pub fn application<'a>(
+        &'a mut self,
+        function: TermRef<'a>,
+        argument: TermRef<'a>,
+    ) -> TermRef<'a> {
+        self.arena
+            .put_no_drop(Term::Application(function, argument))
     }
 }
