00b275e3c07913592b1e1387390300a1c08b411f
Olle Fredriksson <fredriksson.olle@gmail.com>
2024-05-12 12:29:59 +0200

    Ugly shorthands

--- src/syntax.rs
+++ src/syntax.rs
@@ -29,4 +29,16 @@ impl Builder {
         self.arena
             .put_no_drop(Term::Application(function, argument))
     }
+
+    pub fn v<'a>(&'a self, index: usize) -> TermRef<'a> {
+        self.variable(Index(index))
+    }
+
+    pub fn apps<'a>(&'a self, f: TermRef<'a>, args: &[TermRef<'a>]) -> TermRef<'a> {
+        args.iter().fold(f, |f, arg| self.application(f, arg))
+    }
+
+    pub fn l<'a>(&'a self, body: TermRef<'a>) -> TermRef<'a> {
+        self.lambda(body)
+    }
 }
